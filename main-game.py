0349a3bec1aff13c6d4cfed4eea9cabc70f208df
SomethingRandom0768 <49216029+SomethingRandom0768@users.noreply.github.com>
2021-08-05 12:10:28 -0500

    Added models and actors, fixed bug, moved camera

--- main-game.py
+++ main-game.py
@@ -1,5 +1,6 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties
+from direct.actor.Actor import Actor
 
 class Game(ShowBase):
     def __init__(self):
@@ -8,12 +9,33 @@ class Game(ShowBase):
         # Requesting that the window's resolution be (1000, 750)
 
         properties = WindowProperties()
-        properties.setSize(1000, 750)
+        properties.setSize(640, 480)
         self.win.requestProperties(properties)
 
         # Disabling the mouse camera.
         self.disableMouse()
 
+        # Creating the environment model.
+        self.environment = loader.loadModel("PandaSampleModels/Environment/environment")
+        self.environment.reparentTo(render)
+
+        # Creating the Actor with a model and a walk animation.
+        self.tempActor = Actor("models/act_p3d_chan", 
+        {
+            "walk": "models/a_p3d_chan_walk.egg"
+        })
+
+        self.tempActor.reparentTo(render)
+        self.tempActor.getChild(0).setH(180)
+        self.tempActor.loop("walk")
+
+        # Move the camera to a position high above the screen
+        # -- that is, offset it along the z-axis
+        self.camera.setPos(0,0,32)
+        # Tilt the camera down by setting its pitch (the p in hpr)
+        self.camera.setP(-90)
+
+
 
 game = Game()
-game.run
+game.run()
