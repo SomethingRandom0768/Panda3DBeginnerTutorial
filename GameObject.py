f2634f3acd9e90645da529269cefb708493f41c8
SomethingRandom <shishkebab259@gmail.com>
2021-12-10 10:06:38 -0600

    Changing some directories

---
+++ GameObject.py
@@ -0,0 +1,93 @@
+from panda3d.core import Vec3, Vec2
+from direct.actor.Actor import Actor
+from panda3d.core import CollisionSphere, CollisionNode
+
+FRICTION = 150.0
+
+
+class GameObject():
+    def __init__(self, pos, modelName, modelAnims, maxHealth, maxSpeed, colliderName):
+        self.actor = Actor(modelName, modelAnims)
+        self.actor.reparentTo(render)
+        self.actor.setPos(pos)
+
+        self.maxHealth = maxHealth
+        self.health = maxHealth
+
+        self.maxSpeed = maxSpeed
+
+        self.velocity = Vec3(0,0,0)
+        self.acceleration = 300.0
+
+        self.walking = False
+
+        colliderNode = CollisionNode(colliderName)
+        colliderNode.addSolid(CollisionSphere(0, 0, 0, 0.3))
+        self.collider = self.actor.attachNewNode(colliderNode)
+        
+        self.collider.setPythonTag("owner", self)
+
+    def update(self, dt):
+        """If we're going faster than our maximum speed,
+        set the velocity-vector's length to that maximum"""
+        speed = self.velocity.length()
+
+        if speed > self.maxSpeed:
+            self.velocity.normalize()
+            self.velocity *= self.maxSpeed
+            speed = self.maxSpeed
+
+        if not self.walking:
+            frictionVal = FRICTION*dt
+            if frictionVal > speed:
+                self.velocity.set(0,0,0)
+            else:
+                frictionVec = -self.velocity
+                frictionVec.normalize()
+                frictionVec *= frictionVal
+
+                self.velocity += frictionVec
+
+        self.actor.setPos(self.actor.getPos() + self.velocity*dt)
+    
+    def alterHealth(self, dHealth):
+            self.health += dHealth
+
+            if self.health > self.maxHealth:
+                self.health = self.maxHealth
+    
+    def cleanup(self):
+
+        if self.collider is not None and not self.collider.isEmpty():
+            self.collider.clearPythonTag("owner")
+            base.cTrav.removeCollider(self.collider)
+            base.pusher.removeCollider(self.collider)
+
+        if self.actor is not None:
+            self.actor.cleanup()
+            self.actor.removeNode()
+            self.actor = None
+
+        self.collider = None
+
+
+class Player(GameObject):
+    def __init__(self):
+        GameObject.__init__(self, Vec3(0,0,0), "models/act_p3d_chan", {
+            "stand": "models/a_p3d_chan_idle",
+            "walk": "models/a_p3d_chan_run"
+        }, 5, 10, "player")
+
+        # Have panda-chan face forwards.
+        self.actor.getChild(0).setH(180)
+
+        base.pusher.addCollider(self.collider, self.actor)
+        base.cTrav.addCollider(self.collider, base.pusher)
+
+        self.actor.loop("stand")
+
+class Enemy(GameObject):
+    pass
+
+class WalkingEnemy(GameObject):
+    pass
---
+++ main-game.py
@@ -0,0 +1,146 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
+from panda3d.core import AmbientLight
+from panda3d.core import Vec4, Vec3
+from panda3d.core import DirectionalLight
+from panda3d.core import CollisionTraverser, CollisionHandlerPusher, CollisionSphere, CollisionNode
+from panda3d.core import CollisionTube
+from direct.actor.Actor import Actor
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Requesting that the window's resolution be (1000, 750)
+
+        properties = WindowProperties()
+        properties.setSize(640, 480)
+        self.win.requestProperties(properties)
+
+        # Disabling the mouse camera.
+        self.disableMouse()
+
+        # Creating the environment model.
+        self.environment = loader.loadModel("PandaSampleModels/Environment/environment")
+        self.environment.reparentTo(render)
+
+        # Creating the Actor with a model and a walk animation.
+        self.tempActor = Actor("models/act_p3d_chan", 
+        {
+            "walk": "models/a_p3d_chan_walk.egg"
+        })
+
+        self.tempActor.reparentTo(render)
+        self.tempActor.getChild(0).setH(180)
+        self.tempActor.loop("walk")
+
+        # Move the camera to a position high above the screen
+        # -- that is, offset it along the z-axis
+        self.camera.setPos(0,0,32)
+        # Tilt the camera down by setting its pitch (the p in hpr)
+        self.camera.setP(-90)
+
+        ambientLight = AmbientLight("ambient light")
+        ambientLight.setColor(Vec4(0.2, 0.2, 0.2, 1))
+        # Did this to get the node path since we created a node right above.
+        self.ambientLightNodePath = render.attachNewNode(ambientLight)
+        render.setLight(self.ambientLightNodePath)
+
+        mainLight = DirectionalLight("main light")
+        # Same thing here.
+        self.mainLightNodePath = render.attachNewNode(mainLight)
+        # Turn by 45 degrees, then tilt down by 45.
+        self.mainLightNodePath.setHpr(45, -45, 0)
+        render.setLight(self.mainLightNodePath)
+
+        # Applying a shader
+        render.setShaderAuto()
+
+        self.keyMap = {
+            "up": False,
+            "down": False,
+            "left": False,
+            "right": False,
+            "shoot": False
+        }
+               
+        self.accept("w", self.updateKeyMap, ["up", True])
+        self.accept("w-up", self.updateKeyMap, ["up", False])
+        self.accept("s", self.updateKeyMap, ["down", True])
+        self.accept("s-up", self.updateKeyMap, ["down", False])
+        self.accept("a", self.updateKeyMap, ["left", True])
+        self.accept("a-up", self.updateKeyMap, ["left", False])
+        self.accept("d", self.updateKeyMap, ["right", True])
+        self.accept("d-up", self.updateKeyMap, ["right", False])
+        self.accept("mouse1", self.updateKeyMap, ["shoot", True])
+        self.accept("mouse1-up", self.updateKeyMap, ["shoot", False])
+
+        # Task manager code.
+
+        self.updateTask = taskMgr.add(self.update, "update")
+
+        self.cTrav = CollisionTraverser()
+        self.pusher = CollisionHandlerPusher()
+
+        colliderNode = CollisionNode("player")
+
+        colliderNode.addSolid(CollisionSphere(0, 0, 0, 0.3))
+        collider = self.tempActor.attachNewNode(colliderNode)
+        collider.show()
+
+        base.pusher.addCollider(collider, self.tempActor)
+        base.cTrav.addCollider(collider, self.pusher)
+
+        wallSolid = CollisionTube(-8.0, 0, 0, 8.0, 0, 0, 0.2)
+        wallNode = CollisionNode("wall")
+        wallNode.addSolid(wallSolid)
+        wall = render.attachNewNode(wallNode)
+        wall.show()
+        wall.setY(8.0)
+
+        wallSolid = CollisionTube(-8.0, 0, 0, 8.0, 0, 0, 0.2)
+        wallNode = CollisionNode("wall")
+        wallNode.addSolid(wallSolid)
+        wall = render.attachNewNode(wallNode)
+        wall.show()
+        wall.setY(-8.0)
+
+        wallSolid = CollisionTube(0, -8.0, 0, 0, 8.0, 0, 0.2)
+        wallNode = CollisionNode("wall")
+        wallNode.addSolid(wallSolid)
+        wall = render.attachNewNode(wallNode)
+        wall.show()
+        wall.setX(8.0)
+
+        wallSolid = CollisionTube(0, -8.0, 0, 0, 8.0, 0, 0.2)
+        wallNode = CollisionNode("wall")
+        wallNode.addSolid(wallSolid)
+        wall = render.attachNewNode(wallNode)
+        wall.show()
+        wall.setX(-8.0)
+
+    def update(self, task):
+        dt = globalClock.getDt()
+
+        # Use the global time to calculate how much we move.
+
+        if self.keyMap["up"]:
+            self.tempActor.setPos(self.tempActor.getPos() + Vec3(0, 5.0*dt, 0))
+        if self.keyMap["down"]:
+            self.tempActor.setPos(self.tempActor.getPos() + Vec3(0, -5.0*dt, 0))
+        if self.keyMap["left"]:
+            self.tempActor.setPos(self.tempActor.getPos() + Vec3(-5.0*dt, 0, 0))
+        if self.keyMap["right"]:
+            self.tempActor.setPos(self.tempActor.getPos() + Vec3(5.0*dt, 0, 0))
+        if self.keyMap["shoot"]:
+            print("Zap!")
+
+        return task.cont
+        
+    def updateKeyMap(self, controlName, controlState):
+            self.keyMap[controlName] = controlState
+            print(f"{controlName} set to {controlState}")
+        
+
+game = Game()
+game.run()
